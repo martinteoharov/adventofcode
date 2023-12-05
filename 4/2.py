72d52202862f9a78d94a4fa4993b0b9f8713ce42
mteoharov <martin.teoharov@nhs.net>
2023-12-05 10:50:42 +0000

    d4p2

---
+++ 4/2.py
@@ -0,0 +1,33 @@
+import re
+import sys
+import concurrent.futures
+
+cards = [line for line in sys.stdin.readlines()]
+
+
+def parse_input(card):
+    pattern = ":(.*?)\|(.*)$"
+    m = re.findall(pattern, card)[0]
+    w, mine = [int(i) for i in filter(None, m[0].split(" "))], [
+        int(i) for i in filter(None, m[1].split(" "))
+    ]
+    return w, mine
+
+
+def rec_proc(idx, cards):
+    card = cards[idx]
+    w, m = parse_input(card)
+    cl = len([n for n in m if n in w])
+
+    # base statement
+    if cl <= 0:
+        return 1
+
+    return 1 + sum([rec_proc(i, cards) for i in range(idx + 1, idx + cl + 1)])
+
+
+s = 0
+for idx, card in enumerate(cards):
+    s += rec_proc(idx, cards)
+
+print(s)
